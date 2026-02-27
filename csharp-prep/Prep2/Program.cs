30f2d1ff949d7a995980001614d3da990f52c687
Patrick Mullins <Patrickfmullins@gmail.com>
2026-02-26 20:33:46 -0500

    initial prep2 program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,17 @@
 using System;
+// Write a program that determines the letter grade for a course.
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is your grade percentage? ");
+        //intializing grade as a string to pass the int into a string to output the letter grade
+        string grade = Console.ReadLine();
+        // initializing grade number as an int for future grade processing
+        int gradeNumber = int.Parse(grade);
+        //temporary output of grade input to see if it is properly being passed
+        Console.Write(grade);
     }
 }
