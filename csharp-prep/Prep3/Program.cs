4cc164fd6442a8b82b5420212fd9e79d765cd3ce
Patrick Mullins <Patrickfmullins@gmail.com>
2026-02-26 21:46:53 -0500

    created random number logic for guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -15,6 +15,16 @@ class Program
         {
             Console.Write("What is your guess? ");
             guessNumber = int.Parse(Console.ReadLine());
+            // output message to user that they guess the random number correctly
+            if (guessNumber == magicNumber)
+            {
+                Console.WriteLine("Congratulations, You guess the number correctly!");
+            }
+            // creating logic to print to user that the number is lower then the random number
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Try again, higher");
+            }
         }
     }
 }
