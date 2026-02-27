c113ccf8ef75ec3c6e4aa0543680c3951a2851a1
Patrick Mullins <Patrickfmullins@gmail.com>
2026-02-26 20:25:22 -0500

    Finished prep1 program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // initial message to user prmopting for their first name
+        Console.Write("What is your first name? ");
+        // initializing first name variable using input from the console 
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        // displays the user's name in the style of james bond introduction
+        Console.Write("Your name is " + lastName + ", " + firstName + " " + lastName + ".");
     }
 }
