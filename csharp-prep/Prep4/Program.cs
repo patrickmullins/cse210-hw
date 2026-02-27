c2803570412c0a298f60b4516f40ad121974e9a7
Patrick Mullins <Patrickfmullins@gmail.com>
2026-02-26 22:46:44 -0500

    created input and output of sum for program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Transactions;
 
 class Program
 {
@@ -8,6 +10,25 @@ class Program
         List<int> numbers = new List<int>();
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int num = -1;
+        while(num != 0)
+        {
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+            
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
         
     }
 }
