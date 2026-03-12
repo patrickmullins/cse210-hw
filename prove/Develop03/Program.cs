063b804a879b453fe47434a104542ede85ba60f4
Patrick Mullins <Patrickfmullins@gmail.com>
2026-03-12 18:31:36 -0400

    created basic menu for the scripture memorizer program. Some placeholder cases in the switch are present at this time.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        bool keepOnRunning = true;
+        Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+            do
+            {
+            string userInput = Console.ReadLine();
+            
+
+            switch(userInput)
+                {
+                case "1":
+                Console.WriteLine("case 1 test ");
+                break;
+                
+                case "quit":
+                keepOnRunning = false;
+                break;
+                default:
+                Console.WriteLine("Please input a valid response, enter or 'quit' to finish the program. ");
+                break; 
+                }
+                
+            }
+            while (keepOnRunning);
+        }
+        
     }
-}
