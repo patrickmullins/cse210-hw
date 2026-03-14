409bd01c61a77b1f9767cd35e5b03c26802244f5
Patrick Mullins <Patrickfmullins@gmail.com>
2026-03-14 15:42:51 -0400

    Created reference string array and loop to add the words into a string list.

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,8 +3,17 @@ class Scripture
     private Reference _reference;
     private List<Word> _words = new List<Word>();
 
+    // created scripture class to store reference and text strings.
     public Scripture(Reference reference, string text)
     {
-        
+        _reference = reference;
+        // takes the text of the scripture and puts it into an array
+        string[] splitText = text.Split(' ');
+        //loop to read the words in the array and put it into a string list.
+        foreach(string line in splitText)
+        {
+           _words.Add(new Word(line));             
+        }
     }
+
 }
