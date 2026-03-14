1fe36d39d2a489ada4a72623b959243d53e89fc0
Patrick Mullins <Patrickfmullins@gmail.com>
2026-03-14 18:14:19 -0400

    checked for unused libraries and deleted them. Added more scriptures using a word list in the program class and logic that randomly selects the different scriptures

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,21 +1,25 @@
 using System;
-using System.Collections;
-using System.Runtime.InteropServices;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-        string text ="Trust in the Lord with all thine heart and lean not unto thine own understanding";
-        Scripture scripture = new Scripture(reference, text);
+        List<Scripture> scriptureRefs = new List<Scripture>();
+        scriptureRefs.Add(new Scripture(new Reference("proverbs", 3, 5, 6), "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him,and he shall direct thy paths."));
+        scriptureRefs.Add(new Scripture(new Reference("John", 3, 16), "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."));
+        scriptureRefs.Add(new Scripture(new Reference("Philippians", 4, 13), "I can do all things through Christ which strengtheneth me."));
+        scriptureRefs.Add(new Scripture(new Reference("Galatians", 5, 22, 23), "But the fruit of the Spirit is love, joy, peace, longsuffering, gentleness, goodness, faith, Meekness, temperance: against such there is no law."));
+        // using random number generator to pick a random scripture from the list above.
+        Random rand = new Random();
+        int randomIndex = rand.Next(scriptureRefs.Count);
+        Scripture scripture = scriptureRefs[randomIndex];
         bool keepOnRunning = true;
             do
                 {
-               
+               // clears console so most up to date passage of the scripture is available.
                 Console.Clear();
+                //displays the scripture text using the 
                 Console.WriteLine(scripture.GetDisplayText());
-                if (scripture.IsCompletelyHidden() == true)
+                if (scripture.IsCompletelyHidden())
                     {
                         keepOnRunning = false;
                         break;
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,5 +1,3 @@
-using System.Numerics;
-
 class Word
 {
     private string _text;
