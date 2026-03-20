70293d4f3c49b1389544cd37f6b91f4053394d4a
Patrick Mullins <Patrickfmullins@gmail.com>
2026-03-19 21:39:27 -0400

    created basic skeleton of the main menu for the Develop04 Mindfulness program.

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,11 @@
+//using System;
+
+
+//class Activity(Name, description, userSessionDuration)
+//{
+//string _name = Name;
+//string _discription = description;
+//int _userSessionDuration = _userSessionDuration;
+
+//}
+
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool runningProgram = true;
+        
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1. Start breathing activity");
+            Console.WriteLine("2. Start reflection activity");
+            Console.WriteLine("3. Start listing activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            
+            string userInput = Console.ReadLine();
+            switch(userInput)
+            {
+                case "4":
+                runningProgram = false;
+                break;
+
+                default:
+                Console.Write("Invalid Choice, please select a number from 1-4 to choose an option.");
+                Console.ReadKey();
+                break;
+            }
+
+        }
+
+        while(runningProgram);
     }
 }
