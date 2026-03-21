b165e2570ce533ef2dcdf147d0e3100e32516f98
Patrick Mullins <Patrickfmullins@gmail.com>
2026-03-21 15:11:15 -0400

    created list to hold the prompts for the listing activity

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -4,8 +4,15 @@ class ListingActivity : Activity
     {
         
     }
+    private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
     
-
     public override void Run()
 
     {
@@ -13,6 +20,7 @@ class ListingActivity : Activity
         DateTime endTime = DateTime.Now.AddSeconds(_userSessionDuration);
         Console.WriteLine("Get Ready...");
         showCountSession(3);
+        List<string> userInputList = new list<string>
     }
 
 }
